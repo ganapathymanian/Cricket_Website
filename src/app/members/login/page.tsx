fbc8ddfac8280e4e684ca9c3303f98a2a7cb59d1
ganapathymanian <30673268+ganapathymanian@users.noreply.github.com>
2025-11-26 14:51:50 +0000

    Create LoginPage component for member login

---
+++ src/app/members/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from '../../../components/LoginForm';
+
+const LoginPage = () => {
+    return (
+        <div className="login-container">
+            <h1>Member Login</h1>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default LoginPage;
